c029bc6a77f6b5967501430478c2c2d8bc051458
manita <118423514+Manita0z@users.noreply.github.com>
2025-02-15 09:06:53 +0545

    Add files via upload

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using manita_dotnet_assignment.Models;
+
+namespace manita_dotnet_assignment.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
